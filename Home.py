47d953979a49b567a0357e46e5accea08853ff6e
ivr8bt <ianrector97@gmail.com>
2025-05-16 13:08:03 -0500

    new image

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ new_width = int(new_height * aspect_ratio)
 resized_img = img.resize((new_width, new_height))
 col1, col2, col3 = st.columns([1, 2, 1])  # Adjust ratios as needed
 with col2:
-    st.image(resized_img, use_column_width=False)
+    st.image(resized_img, use_container_width=False)
 
 st.markdown(
     """
