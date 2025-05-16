62dd0569b32fc022b71e45387017af59714f92aa
ivr8bt <ianrector97@gmail.com>
2025-05-16 13:15:08 -0500

    sidebar nav

---
+++ pages/3_🛢️_SQL_Upload.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+        .stApp {
+            background-color: #fff9b0;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("Database Upload")
