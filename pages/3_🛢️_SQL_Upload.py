8c26b5ffbdb7051b53ba04003f64d23a70931538
ivr8bt <ianrector97@gmail.com>
2025-05-17 14:32:38 -0500

    new theme

--- pages/3_🛢️_SQL_Upload.py
+++ pages/3_🛢️_SQL_Upload.py
@@ -1,31 +1,30 @@
 import streamlit as st
-#import pyodbc
-
-st.markdown(
-    """
-    <style>
-        .stApp {
-            background-color: #fff9b0;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+import pandas as pd
+#import pyodbc You will need this in the actual implementation
 
 st.title("Database Upload")
 
- # Show column selection
-st.subheader("Select Columns to Display")
-selected_columns = st.multiselect("Choose columns", df.columns.tolist(), default=df.columns.tolist())
-st.dataframe(df[selected_columns])
+
+if "uploaded_df" in st.session_state:
+    df = st.session_state["uploaded_df"]
+    # Show column selection
+    st.subheader("Select Columns to Display")
+    selected_columns = st.multiselect("Choose columns", df.columns.tolist(), default=df.columns.tolist())
+    st.dataframe(df[selected_columns])
 
 
-# secrets = st.secrets["database"]
+    # You will need all this in the actual implementation with the SQL Server database
+    
+    # secrets = st.secrets["database"]
 
-# conn = pyodbc.connect(
-#     f'DRIVER={{ODBC Driver 17 for SQL Server}};'
-#     f'SERVER={secrets.server};'
-#     f'DATABASE={secrets.database};'
-#     f'UID={secrets.username};'
-#     f'PWD={secrets.password}'
-# )
+    # conn = pyodbc.connect(
+    #     f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+    #     f'SERVER={secrets.server};'
+    #     f'DATABASE={secrets.database};'
+    #     f'UID={secrets.username};'
+    #     f'PWD={secrets.password}'
+    # )
+
+else:
+    st.warning("Please upload a file first on the Upload page.")
+
